c04fb96e849c1530ee7e2ff287fa04820d9c7500
Souleymane Dia <sould32@vt.edu>
2016-06-19 18:55:02 -0400

    Add files via upload

---
+++ Binary-Search-Tree/BinarySearchTree (2).java
@@ -0,0 +1,309 @@
+package binarySearchTree;
+
+// -------------------------------------------------------------------------
+/**
+ * Implements an unbalanced binary search tree. Note that all "matching" is
+ * based on the compareTo() method.
+ *
+ * @param <T>
+ *            The type of data element contained in the node.
+ * @author Mark Allen Weiss (with modifications by John Lewis)
+ * @author <Souleymane Dia> <sould32> (~implementation)
+ * @version <12/05/2015>
+ */
+public class Lab13BinarySearchTree<T extends Comparable<? super T>>
+        implements BinarySearchTree<T> {
+    // ~ Instance/static variables .............................................
+
+    private BinaryNode<T> root;
+
+    // ~ Constructor ...........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Constructs an empty tree.
+     */
+    public Lab13BinarySearchTree() {
+        root = null;
+    }
+
+    // ~ Public methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Insert into the tree.
+     *
+     * @param x
+     *            the item to insert.
+     * @throws DuplicateItemException
+     *             if x is already present.
+     */
+    public void insert(T x) {
+        root = insert(x, root);
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Remove the specified value from the tree.
+     *
+     * @param x
+     *            the item to remove.
+     * @throws ItemNotFoundException
+     *             if x is not found.
+     */
+    public void remove(T x) {
+        root = remove(x, root);
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Find the smallest item in the tree.
+     *
+     * @return The smallest item, or null if the tree is empty.
+     */
+    public T findMin() {
+        return elementAt(findMin(root));
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Find the largest item in the tree.
+     *
+     * @return The largest item in the tree, or null if the tree is empty.
+     */
+    public T findMax() {
+        BinaryNode<T> larg = root;
+        return elementAt(findMax(larg));
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Find an item in the tree.
+     *
+     * @param x
+     *            the item to search for.
+     * @return the matching item or null if not found.
+     */
+    public T find(T x) {
+        return elementAt(find(x, root));
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Make the tree logically empty.
+     */
+    public void makeEmpty() {
+        root = null;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Test if the tree is logically empty.
+     *
+     * @return true if empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to get element value stored in a tree node, with safe
+     * handling of null nodes.
+     *
+     * @param node
+     *            the node.
+     * @return the element field or null if node is null.
+     */
+    private T elementAt(BinaryNode<T> node) {
+        return (node == null) ? null : node.getElement();
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to insert a value into a subtree.
+     *
+     * @param x
+     *            the item to insert.
+     * @param node
+     *            the node that roots the subtree.
+     * @return the new root of the subtree.
+     * @throws DuplicateItemException
+     *             if x is already present.
+     */
+    private BinaryNode<T> insert(T x, BinaryNode<T> node) {
+        if (node == null) {
+            return new BinaryNode<T>(x);
+        }
+        // insert
+        else if (x.compareTo(node.getElement()) < 0) {
+            node.setLeft(insert(x, node.getLeft()));
+        }
+        // insert
+        else if (x.compareTo(node.getElement()) > 0) {
+            node.setRight(insert(x, node.getRight()));
+        }
+        // insert
+        else {
+            throw new DuplicateItemException(x.toString());
+        }
+        return node;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to remove a specified item from a subtree.
+     *
+     * @param x
+     *            the item to remove.
+     * @param node
+     *            the node that roots the subtree.
+     * @return the new root of the subtree.
+     * @throws ItemNotFoundException
+     *             if x is not found.
+     */
+    private BinaryNode<T> remove(T x, BinaryNode<T> node) {
+        // This local variable will contain the new root of the subtree,
+        // if the root needs to change.
+        BinaryNode<T> result = node;
+
+        // If there's no more subtree to examine
+        if (node == null) {
+            throw new ItemNotFoundException(x.toString());
+        }
+
+        // if value should be to the left of the root
+        if (x.compareTo(node.getElement()) < 0) {
+            node.setLeft(remove(x, node.getLeft()));
+        }
+        // if value should be to the right of the root
+        else if (x.compareTo(node.getElement()) > 0) {
+            node.setRight(remove(x, node.getRight()));
+        }
+        // If value is on the current node
+        else {
+            // If there are two children
+            if (node.getLeft() != null && node.getRight() != null) {
+                BinaryNode<T> removed = findMax(node.getLeft());
+                node.setElement(elementAt(removed));
+                node.setLeft(removeLargest(node.getLeft()));
+            }
+            // If there is only one child on the left
+            else if (node.getLeft() != null) {
+                result = node.getLeft();
+            }
+            // If there is only one child on the right
+            else {
+                result = node.getRight();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param node
+     *            no be set to the next
+     * @return the removed
+     */
+    private BinaryNode<T> removeLargest(BinaryNode<T> node) {
+        if (node.getRight() != null) {
+            node.setRight(removeLargest(node.getRight()));
+        }
+        // remove the left
+        else {
+            node = node.getLeft();
+        }
+        return node;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to find the smallest item in a subtree.
+     *
+     * @param node
+     *            the node that roots the tree.
+     * @return node containing the smallest item.
+     */
+    private BinaryNode<T> findMin(BinaryNode<T> node) {
+        if (node == null) {
+            return node;
+        }
+        // case 1
+        else if (node.getLeft() == null) {
+            return node;
+        }
+        // case 2
+        else {
+            return findMin(node.getLeft());
+        }
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to find the largest item in a subtree.
+     *
+     * @param node
+     *            the node that roots the tree.
+     * @return node containing the largest item.
+     */
+    private BinaryNode<T> findMax(BinaryNode<T> node) {
+        if (node == null) {
+            return node;
+        }
+        // case 1
+        else if (node.getRight() == null) {
+            return node;
+        }
+        // case 2
+        else {
+            return findMax(node.getRight());
+        }
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Internal method to find an item in a subtree.
+     *
+     * @param x
+     *            is item to search for.
+     * @param node
+     *            the node that roots the tree.
+     * @return node containing the matched item.
+     */
+    private BinaryNode<T> find(T x, BinaryNode<T> node) {
+        if (node == null) {
+            return null; // Not found
+        }
+        // find
+        else if (x.compareTo(node.getElement()) < 0) {
+            // Search in the left subtree
+            return find(x, node.getLeft());
+        }
+        // find
+        else if (x.compareTo(node.getElement()) > 0) {
+            // Search in the right subtree
+            return find(x, node.getRight());
+        }
+        // find
+        else {
+            return node; // Match
+        }
+    }
+
+    /**
+     * Gets an in-order string representation of the tree If the tree holds 5 /
+     * \ 2 6 \ 3 It would print (2, 3, 5, 6)
+     * 
+     * @return an in-order string representation of the tree
+     */
+    @Override
+    public String toString() {
+        if (root == null) {
+            return "()";
+        }
+        // str
+        else {
+            return "(" + root.toString() + ")";
+        }
+    }
+}
